a2335b57cc489bb7dfb66bf12eeca1e10e931349
Manish Prajapati <32140233+mpraj@users.noreply.github.com>
2018-01-24 01:06:57 +0530

    Add files via upload

---
+++ template/js_files.php
@@ -0,0 +1,8 @@
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="js/popper.min.js"> </script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.cookie.js"> </script>
+<script src="js/jquery.validate.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
+<script src="js/charts-home.js"></script>
+<script src="js/front.js"></script>
