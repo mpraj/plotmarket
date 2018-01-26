54a359c22633ea8142282aba03616f81a69179cd
Manish Prajapati <32140233+mpraj@users.noreply.github.com>
2018-01-26 19:43:50 +0530

    Add files via upload

--- userSignup.php
+++ userSignup.php
@@ -1,17 +1,32 @@
 <?php include_once('template/header.php') ?>
 
-<div class="page">
-<section class="forms">
-    <div class="container-fluid">
+<div class="page login-page">
+<section class="container d-flex align-items-center">
+    <div class="form-holder has-shadow">
         <div class="row">
-            <!-- Form Elements -->
-            <div class="col-lg-6">
-                <div class="card">
 
-                    <div class="card-header d-flex align-items-center">
-                        <h3 class="h4">Register</h3>
+            <div class="col-lg-6">
+                <div class="info d-flex align-items-center">
+                    <div class="content">
+                        <div class="logo">
+                            <h1><small>Plot</small>Market</h1>
+                        </div>
+                        <p>your Dreamland portal</p>
                     </div>
-                    <div class="card-body">
+                </div>
+            </div>
+            <!-- Form Elements -->
+            <div class="col-lg-6 bg-white">
+                <div class="form d-flex align-items-center">
+
+                    <!--<div class="card-header d-flex align-items-center">
+                        <h1><i class="fa fa-user-plus"></i>&nbsp;Register</h1>
+                    </div>-->
+                    <div class="content">
+                    <div class="modal-header p-0 mb-4">
+                        <h1><i class="fa fa-user-plus"></i> Register</h1>
+                    </div>
+                    <div class="content">
                         <div class="row p-4">
                             <!--<div class="col-5">
                             <div class="align-items-center nav-item">
@@ -29,13 +44,13 @@
                                 </div>
                             </div>-->
                             <!-- Nav tabs -->
-                            <ul class="nav nav-tabs">
+                            <ul class="nav nav-tabs" role="tablist">
                                 <li class="nav-item">
-                                    <a class="dropdown-item active px-5 py-2" data-toggle="tab" href="#buyer">
+                                    <a class="nav-link active px-5 py-2" data-toggle="tab" href="#buyer">
                                         Buyer</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="dropdown-item px-5 py-2" data-toggle="tab" href="#seller">
+                                    <a class="nav-link px-5 py-2" data-toggle="tab" href="#seller">
                                         Seller</a>
                                 </li>
                             </ul>
@@ -46,9 +61,10 @@
                                 <div class="tab-pane container" id="seller">hello</div>
                             </div>-->
                         </div>
+                        <div class="line"></div>
 
                         <div class="tab-content">
-                        <form class="form-horizontal tab-pane active container" id="buyer" name="buyer" method="post">
+                        <form class="form-horizontal tab-pane active" id="buyer" name="buyer" method="post">
                             <div class="form-group row">
                                 <label class="col-sm-3 form-control-label">Name</label>
                                 <div class="col-sm-9">
@@ -66,14 +82,20 @@
                             <div class="form-group row">
                                 <label class="col-sm-3 form-control-label">Mobile</label>
                                 <div class="col-sm-9">
-                                    <input type="text" class="form-control">
+                                    <div class="form-group m-0">
+                                        <div class="input-group">
+                                            <div class="input-group-prepend"><span class="input-group-text">+91</span></div>
+                                            <input type="text" placeholder="Mobile" class="form-control">
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
+
                             <div class="line"></div>
                             <div class="form-group row">
                                 <label class="col-sm-3 form-control-label">Email</label>
                                 <div class="col-sm-9">
-                                    <input type="email" class="form-control">
+                                    <input type="text" class="form-control">
                                 </div>
                             </div>
                             <div class="form-group row">
@@ -93,13 +115,14 @@
                             <div class="line"></div>
                             <div class="form-group row">
                                 <div class="col-sm-6 offset-sm-3">
-                                    <button type="reset" class="btn btn-secondary">Cancel</button>
                                     <button type="submit" class="btn btn-primary">Signup</button>
+                                    <button type="reset" class="btn btn-secondary">Cancel</button>
+
                                 </div>
                             </div>
                         </form>
 
-                            <form class="form-horizontal tab-pane container" id="seller" name="seller" method="post">
+                            <form class="form-horizontal tab-pane" id="seller" name="seller" method="post">
                                 <div class="form-group row">
                                     <label class="col-sm-3 form-control-label">Name</label>
                                     <div class="col-sm-9">
@@ -112,6 +135,19 @@
                                         <input type="text" class="form-control">
                                     </div>
                                 </div>
+                                <div class="line"></div>
+                                <div class="form-group row">
+                                    <label class="col-sm-3 form-control-label">Mobile</label>
+                                    <div class="col-sm-9">
+                                        <div class="form-group m-0">
+                                            <div class="input-group">
+                                                <div class="input-group-prepend"><span class="input-group-text">+91</span></div>
+                                                <input type="text" placeholder="Mobile" class="form-control">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="line"></div>
                                 <div class="form-group row">
                                     <label class="col-sm-3 form-control-label">Address</label>
                                     <div class="col-sm-9">
@@ -130,17 +166,7 @@
                                         <input type="text" class="form-control">
                                     </div>
                                 </div>
-                                <div class="form-group row">
-                                    <label class="col-sm-3 form-control-label">Mobile</label>
-                                    <div class="col-sm-9">
-                                        <div class="form-group">
-                                            <div class="input-group">
-                                                <div class="input-group-prepend"><span class="input-group-text">+91</span></div>
-                                                <input type="text" placeholder="Mobile" class="form-control">
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+
                                 <div class="line"></div>
                                 <div class="form-group row">
                                     <label class="col-sm-3 form-control-label">Plot Location</label>
@@ -148,7 +174,6 @@
                                         <input type="text" class="form-control">
                                     </div>
                                 </div>
-                                
                                 <div class="form-group row">
                                     <label for="sel1" class="col-sm-3 form-control-label">Type:</label>
                                     <div class="col-sm-9">
@@ -158,7 +183,7 @@
                                         </select>
                                     </div>
                                 </div>
-
+                                <div class="line"></div>
                                 <div class="form-group row">
                                     <label class="col-sm-3 form-control-label">Aadhaar Upload</label>
                                     <div class="col-sm-9">
@@ -191,13 +216,14 @@
                                 <div class="line"></div>
                                 <div class="form-group row">
                                     <div class="col-sm-6 offset-sm-3">
-                                        <button type="submit" class="btn btn-secondary">Cancel</button>
                                         <button type="submit" class="btn btn-primary">Signup</button>
+                                        <button type="submit" class="btn btn-secondary">Cancel</button>
                                     </div>
                                 </div>
                             </form>
                         </div>
                     </div>
+                </div>
                 </div>
             </div>
         </div>
