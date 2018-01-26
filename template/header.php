97db7e63bc55d478618e47a0f3739dd6cb93346f
Manish Prajapati <32140233+mpraj@users.noreply.github.com>
2018-01-26 19:44:22 +0530

    Add files via upload

--- template/header.php
+++ template/header.php
@@ -9,17 +9,17 @@
     <meta name="robots" content="all,follow">
     <!-- Bootstrap CSS-->
     <!--    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">-->
-    <link rel="stylesheet" href="https://d19m59y37dris4.cloudfront.net/admin-premium/1-2/vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://d19m59y37dris4.cloudfront.net/admin/1-3-2/vendor/bootstrap/css/bootstrap.min.css">
     <!-- Fontastic Custom icon font-->
-    <link rel="stylesheet" href="css/fontastic.css">
+    <link rel="stylesheet" href="/css/fontastic.css">
     <!-- Font Awesome CSS-->
-    <link rel="stylesheet" href="../css/font-awesome.min.css">
+    <link rel="stylesheet" href="/css/font-awesome.min.css">
     <!-- Google fonts - Poppins -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,700">
     <!-- theme stylesheet-->
-    <link rel="stylesheet" href="css/style.default.css" id="theme-stylesheet">
+    <link rel="stylesheet" href="/css/style.default.css" id="theme-stylesheet">
     <!-- Custom stylesheet - for your changes-->
-    <link rel="stylesheet" href="css/custom.css">
+    <link rel="stylesheet" href="/css/custom.css">
     <!-- Favicon-->
     <link rel="shortcut icon" href="favicon.png">
     <!-- Tweaks for older IEs--><!--[if lt IE 9]>
