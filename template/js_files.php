e76e16d5ec8a4baf764b67f81b602d4cdc2704e5
Manish Prajapati <32140233+mpraj@users.noreply.github.com>
2018-01-26 19:44:51 +0530

    Add files via upload

--- template/js_files.php
+++ template/js_files.php
@@ -1,6 +1,8 @@
 <script src="js/jquery-3.2.1.min.js"></script>
-<script src="js/popper.min.js"> </script>
-<script src="js/bootstrap.min.js"></script>
+<!--<script src="js/popper.min.js"> </script>
+<script src="js/bootstrap.min.js"></script>-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 <script src="js/jquery.cookie.js"> </script>
 <script src="js/jquery.validate.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
